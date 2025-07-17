92ea88cc3ca04ed318e08141276988a04ec307c2
Grigoriy <foxtail25@gmail.com>
2025-07-17 09:43:22 +0300

    auth - начало

--- public/view/pages/slug1.php
+++ public/view/pages/slug1.php
@@ -1,7 +1,10 @@
 <?php
 
-$content = 'slug1';
-$content .= $params['slug1'];
+$content = '';
+if($params['slug1'] == 'auth' or $params['slug1'] == 'auth') {
+    $authForm = file_get_contents('view/template/auth.html');
+    $content .=$authForm;
+}
 $content .='<a href="/">вернуться на главную</a>';
 
 return ['title' => 'slug1', 'content' => $content];
