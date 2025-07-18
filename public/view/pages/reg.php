3f4c4d1138276dfd330c322b451dd4e3c42d8390
Fox Tail <foxtail25@gmail.com>
2025-07-18 19:32:33 +0300

    auth error

--- public/view/pages/reg.php
+++ public/view/pages/reg.php
@@ -7,16 +7,22 @@ if(!empty($_POST['login'])) {
 				$_SESSION['user']['auth'] = true;
 				$_SESSION['user']['name'] = $_POST['login'];
 				die();
+			} else {
+				return '<p>повторный пароль не введён</p>
+				<p><a href="/page/reg">заново</a></p>';			
 			}
+		} else {
+			return '<p> не указан пароль</p>
+			<p><a href="/page/reg">заново</a></p>';	
 		}
+	} else {
+		// $_POST = null;
+		return '<p> не указан емаил</p>
+		<p><a href="/page/reg">заново</a></p>';
 	}
 } else {
-	
 	return file_get_contents('view/template/reg.html');
 }
-// if(isset($regForm)){
 
-// 	return $regForm;
-// }
 
 ?>
