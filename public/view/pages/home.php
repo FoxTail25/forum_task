7284836167d26c34f309f2794686bc2974dbf010
Fox Tail <foxtail25@gmail.com>
2025-07-22 12:34:10 +0300

    topic

--- public/view/pages/home.php
+++ public/view/pages/home.php
@@ -1,8 +1,24 @@
 <?php
+require('db/connect.php');
+
 $title = 'home title';
 $content = '';
+$err = null;
+if(isset($_POST['new_topic'])){
+	$query_check = "SELECT * FROM topics WHERE name = '$_POST[new_topic]'";
+	$res = mysqli_fetch_assoc(mysqli_query($link, $query_check));
+	if(isset($res)) {
+		$err = 'Такая тема уже есть';
+	} else {
+		$query_ad = "INSERT INTO topics (slug, name) VALUES ('$_POST[new_topic]', '$_POST[new_topic]')";
+		mysqli_query($link, $query_ad);
+		$_POST['new_topic'] = null;
+		unset($_POST['new_topic']);
+		header('Location: /');
+		die();
+	}
+}
 
-require('db/connect.php');
 
 $query = "SELECT * FROM topics";
 $res = mysqli_query($link, $query);
@@ -21,11 +37,26 @@ $content .= '<div><h5 class="text-center">Список тем</h5>';
 
 if(count($data) > 0) {
 foreach($data as $elem){
-	$content .= "<span class=\"ps-2\">$elem[name]</span>";
+	$content .= "<a class=\"ps-2\" href=\"page/$elem[slug]\">$elem[name]</a>";
 }
 } else {
 	$content .= "<p class=\"text-center\">Темы пока отсутствуют :(</p>";
 }
+if(isset($_SESSION['user'])) {
+	if(!$err) {
+
+		$content .= '<h6>Создать новую тему</h6>
+		<form method="POST">
+		<label>Новая тема
+		<input type="text" name="new_topic">
+		</label>
+		<input type="submit" value="создать">
+		</form>';
+		} else {
+			$content .= "<p>Такая тема уже есть</p>";
+			$content .= "<a href=\"/\">Попробовать ещё раз</a>";
+		}
+} 
 $content .='</div>';
 
 return ['title' => $title, 'content'=> $content];
--- public/view/pages/slug1.php
+++ public/view/pages/slug1.php
@@ -3,13 +3,21 @@
 $content = '';
 if($params['slug1'] == 'auth') {
     $content .= include('auth.php');
-}
-if($params['slug1'] == 'reg') {
+} else if($params['slug1'] == 'reg') {
     $content .= include('reg.php');
-}
-if($params['slug1'] == 'logoff') {
+} else if($params['slug1'] == 'logoff') {
     require('logoff.php');
+} else {
+    include('db/connect.php');
+    $query = "SELECT * FROM topics";
+    $res = mysqli_query($link, $query);
+    for($data=[]; $row = mysqli_fetch_assoc($res); $data[] = $row['slug']);
+
+    if(in_array($params['slug1'], $data)) {
+        $content .= "<p>$params[slug1]</p>";
+    } 
 }
+
 
 
 $content .='<p class="text-center"><a href="/">вернуться на главную</a></p>';
