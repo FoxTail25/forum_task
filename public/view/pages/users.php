747cf7a99d671ead89e866be068114e033ffc019
Fox Tail <foxtail25@gmail.com>
2025-07-29 10:42:18 +0300

    userBan complite

--- public/view/pages/users.php
+++ public/view/pages/users.php
@@ -1,4 +1,29 @@
-<?php 
+<?php
+
+if(isset($_POST['user_id'])) {
+	$userId = $_POST['user_id'];
+	if($_POST['user_ban']) {
+		$userBan = 0;
+	}else {
+		$userBan = 1;
+	};
+
+	// var_dump($_POST);
+	include('db/connect.php');
+
+	$query = "UPDATE user SET ban = '$userBan' WHERE id = '$userId'";
+	mysqli_query($link, $query);
+
+	$_POST['user_id'] = null;
+	$_POST['user_ban'] = null;
+	unset($_POST['user_id']);
+	unset($_POST['user_ban']);
+	header('Location:/page/users');
+	die();
+}
+
+
+
 include('db/connect.php');
 $query = "SELECT
 user.id as user_id, 
@@ -16,62 +41,40 @@ $db_answer = mysqli_query($link, $query);
 
 for($data = []; $row = mysqli_fetch_assoc($db_answer); $data[]=$row);
 
-// var_dump($data);
-
 $con = "<h1>Таблица пользователей</h1>";
 
-// $con .= '<form method="POST">';
 $con .= '<table>';
-$con .= '<thead><tr><th>user_id</th><th>user_name</th><th>user_role</th><th>user_ban</th><th>change</th></tr></thead>';
+$con .= '<thead><tr><th>user_id</th><th>user_name</th><th>user_role</th><th>user_ban</th><th>change_ban</th></tr></thead>';
 $con .= '<tbody>';
-// foreach($data as $user){
-// 	$ban = $user['user_ban'];
-// 	$neban = !$ban;
-// 	$con .= '<tr>';	
-// 	$con .= "<td>$user[user_id]</td>"
-// 	."<td>$user[user_name]</td>"
-// 	."<td>$user[user_role]</td>"
-// 	// ."<td>$user[user_ban]</td>";
-// 	."<td><select>
-// 	<option value=\"$ban\">нет</option>
-// 	<option value=\"$neban\">да</option>
-// 	</select></td>";
-// 	$con .= '</tr>';
-// }
+
 foreach($data as $user){
+
 	$con .= '<tr>';
 		$con .= "<td>$user[user_id]</td>";
 		$con .= "<td>$user[user_name]</td>";
 		$con .= "<td>$user[user_role]</td>";
-		$con .= "<td>$user[user_ban]</td>";
-		$con .= '';
+
+		if($user['user_ban']) {
+			$con .= "<td>да</td>";
+		} else {
+			$con .= "<td>нет</td>";
+		}
+
+		$con .= '<td><form method="POST">
+		<input class="d-none" name="user_id" value="'.$user['user_id'].'">
+		<input class="d-none" name="user_ban" value="'.$user['user_ban'].'">';
+
+		if($user['user_ban']) {
+			$con .= '<input type="submit" value="разбанить">';
+		} else {
+			$con .= '<input type="submit" value="забанить">';
+		}
+
+		$con .= '</form></td>';
 		$con .= '</tr>';
 
 }
 $con .= '</tbody>';
 $con .= '</table>';
-// $con .= '</form>';
 return $con;
 ?>
-<!-- 
-
-$con = "<h1>Таблица пользователей</h1>";
-$con .= '<table>';
-$con .= '<thead><tr><th>user_id</th><th>user_name</th><th>user_role</th><th>user_ban</th></tr></thead>';
-$con .= '<tbody>';
-foreach($data as $user){
-	$ban = $user['user_ban'];
-	$neban = !$ban;
-	$con .= '<tr>';	
-	$con .= "<td>$user[user_id]</td>"
-	."<td>$user[user_name]</td>"
-	."<td>$user[user_role]</td>"
-	// ."<td>$user[user_ban]</td>";
-	."<td><select>
-	<option value=\"$ban\">нет</option>
-	<option value=\"$neban\">да</option>
-	</select></td>";
-	$con .= '</tr>';
-}
-$con .= '</tbody>';
-$con .= '</table>'; -->
